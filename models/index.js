652945b0838f1b0f9f31f74392e40c66f2aec9f8
pankajsharmamails@gmail.com <rj23cb4888>
2020-05-29 15:38:35 +0530

    Atlas mongodb connected

--- models/index.js
+++ models/index.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Phonebook', { useNewUrlParser: true, useFindAndModify: false }, (err) => {
+// mongoose.connect('mongodb://localhost:27017/Phonebook', { useNewUrlParser: true, useFindAndModify: false }, (err) => {
+//     if (!err) { console.log('MongoDB Connection Succeeded.') }
+//     else { console.log('Error in DB connection : ' + err) }
+// });
+
+mongoose.connect("mongodb+srv://master:master123@cluster0-8kidn.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useFindAndModify: false }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
+
+// const uri = "mongodb+srv://mongouser:<password>@cluster0-8kidn.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
 
 require('./person.model');
